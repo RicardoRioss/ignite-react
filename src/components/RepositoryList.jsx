db766aa4a3c046a6c51336dcd2e2baeb525aef5a
Ricardo Rios <rickrios23@hotmail.com>
2022-02-27 10:01:50 -0300

    Adicionando o ESLint

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,21 +1,26 @@
+
+import { useState, useEffect} from 'react';
 import { RepositoryItem } from "./RepositoryItem"
 import '../styles/repositories.scss'
-const repository = {
-  name: 'unform',
-  description: 'Form In React',
-  link: 'https://github.com/RicardoRioss/ignite-react'
-}
-
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]) 
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/RicardoRioss/repos')
+    .then(response => response.json())
+    .then(data =>setRepositories(data))
+  }, [])
+
   return (
     <section className="repository-list" >
       <h1>Lista de repositórios</h1>
       <ul>
-        <RepositoryItem  repository = {repository}/>
-        <RepositoryItem repository = {repository}/>
-        <RepositoryItem repository = {repository}/>
-        <RepositoryItem repository = {repository}/>
+
+       {repositories.map(repository => {
+         return  <RepositoryItem key = {repository.name}  repository = {repository}/>
+       })}
+
       </ul>
     </section>
   )
